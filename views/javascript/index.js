e42f0263e9fc8c0b5953b44b3d31383486a09175
Mohammed Chisti <93mchisti@gmail.com>
2018-10-21 01:20:05 -0400

    Simple API fetch request

--- views/javascript/index.js
+++ views/javascript/index.js
@@ -1,1 +1,28 @@
-function itemSubmit() {}
+document.getElementById('itemAddForm').addEventListener('submit', e => {
+  e.preventDefault()
+
+  let headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+
+  let body = JSON.stringify({
+    name: "Apple",
+    price: 0.59,
+    unitPrice: 0.59,
+    unit: "apple",
+    location: "Ideals, 100-48",
+    date: "18102018",
+  })
+
+  fetch("http://localhost:5000/api/items", {
+    method: "POST",
+    headers,
+    body
+  })
+  .then(response => response.json())
+  .then(item => {
+    document.getElementById("root").innerHTML = item.name
+  })
+  .catch(err => new Error(err.message))
+})
